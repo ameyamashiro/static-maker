9041e7cdd37b2eab74f6c5c1370f110f5cb8ac58
ameyamashiro <yamashiro@ryukyu-i.co.jp>
2018-03-19 12:21:30 +0900

    add: accept multiple rsync configs

--- admin/partials/static-maker-admin-display-setup.php
+++ admin/partials/static-maker-admin-display-setup.php
@@ -41,6 +41,7 @@ namespace Static_Maker;
             'before_command' => ''
         )
     );
+    global $rsync_options;
     $rsync_options = isset( $options[ 'rsync' ] ) ? $options[ 'rsync' ] : $rsync_initial;
     ?>
 
@@ -105,54 +106,12 @@ namespace Static_Maker;
                     </td>
                 </tr>
                 <tr>
-                    <th scope="row">rsync</th>
-                    <td>
-                        <?php foreach ( $rsync_options as $i => $rsync ): ?>
-                        <ul>
-                            <li>
-                                <label for="<?php echo $this->plugin_name ?>-rsync-options[<?php echo $i ?>][host]">
-                                    Host:
-                                    <input type="text" id="<?php echo $this->plugin_name ?>-rsync-options[<?php echo $i ?>][host]" name="<?php echo $this->plugin_name ?>[rsync][<?php echo $i ?>][host]" value="<?php echo $rsync['host'] ?>" class="regular-text" placeholder="IP or Host Domain Name">
-                                </label>
-                            </li>
-                            <li>
-                                <label for="<?php echo $this->plugin_name ?>-rsync-options[<?php echo $i ?>][user]">
-                                    User:
-                                    <input type="text" id="<?php echo $this->plugin_name ?>-rsync-options[<?php echo $i ?>][user]" name="<?php echo $this->plugin_name ?>[rsync][<?php echo $i ?>][user]" value="<?php echo $rsync['user'] ?>" class="regular-text" placeholder="ec2-user etc..">
-                                </label>
-                            </li>
-                            <li>
-                                <label for="<?php echo $this->plugin_name ?>-rsync-options[<?php echo $i ?>][ssh_key]">
-                                    <p>SSH Private Key:</p>
-                                    <textarea id="<?php echo $this->plugin_name ?>-rsync-options[<?php echo $i ?>][ssh_key]" class="large-text code" name="<?php echo $this->plugin_name ?>[rsync][<?php echo $i ?>][ssh_key]"><?php echo CryptoUtil::decrypt( $rsync['ssh_key'], true ) ?></textarea>
-                                </label>
-                            </li>
-                            <li>
-                                <label for="<?php echo $this->plugin_name ?>-rsync-options[<?php echo $i ?>][dir]">
-                                    Target Directory:
-                                    <input type="text" id="<?php echo $this->plugin_name ?>-rsync-options[<?php echo $i ?>][dir]" name="<?php echo $this->plugin_name ?>[rsync][<?php echo $i ?>][dir]" class="regular-text" placeholder="~/public" value="<?php echo $rsync['dir'] ?>">
-                                </label>
-                            </li>
-                            <li>
-                                <label for="<?php echo $this->plugin_name ?>-rsync-options[<?php echo $i ?>][rsync_options]">
-                                    Additional rsync options:
-                                    <input type="text" id="<?php echo $this->plugin_name ?>-rsync-options[<?php echo $i ?>][rsync_options]" name="<?php echo $this->plugin_name ?>[rsync][<?php echo $i ?>][rsync_options]" class="regular-text" placeholder="--exclude &quot;.git&quot;" value="<?php echo $rsync['rsync_options'] ?>">
-                                </label>
-                            </li>
-                            <li>
-                                <label for="<?php echo $this->plugin_name ?>-rsync-options[<?php echo $i ?>][before_command]">
-                                    Before command:
-                                    <input type="text" id="<?php echo $this->plugin_name ?>-rsync-options[<?php echo $i ?>][before_command]" name="<?php echo $this->plugin_name ?>[rsync][<?php echo $i ?>][before_command]" class="regular-text" placeholder="cp -r " value="<?php echo $rsync['before_command'] ?>">
-                                </label>
-                                <p class="description">Command which will be executed before rsync</p>
-                                <p class="description">
-                                    {{ROOT}} : Document root (get_home_path())<br>
-                                    {{WP_ROOT}} : WordPress root (ABSPATH)<br>
-                                    {{OUTPUT_DIR}} : Static Maker output directory
-                                </p>
-                            </li>
-                        </ul>
-                        <?php endforeach; ?>
+                    <th scope="row" data-rsync-count="<?php echo count($rsync_options) ?>">rsync</th>
+                    <td class="rsync-list">
+                        <!-- rsync list is inserted here -->
+
+                        <button type="button" class="add-target button-primary" data-sm-target="rsync">Add rsync target</button>
+                        <button type="button" class="remove-target button" data-sm-target="rsync">Remove</button>
                     </td>
                 </tr>
             </tbody>
@@ -161,12 +120,118 @@ namespace Static_Maker;
         <?php submit_button(__('Save all changes', $this->plugin_name), 'primary','submit', TRUE); ?>
     </form>
 
+    <script type="text/html" id="rsync-template">
+        <ul data-sm-source="rsync" class="rsync-item">
+            <li>
+                <label for="<?php echo $this->plugin_name ?>-rsync-options[{{COUNT}}][host]">
+                    Host:
+                    <input type="text" id="<?php echo $this->plugin_name ?>-rsync-options[{{COUNT}}][host]" name="<?php echo $this->plugin_name ?>[rsync][{{COUNT}}][host]" value="{{HOST}}" class="regular-text" placeholder="IP or Host Domain Name">
+                </label>
+            </li>
+            <li>
+                <label for="<?php echo $this->plugin_name ?>-rsync-options[{{COUNT}}][user]">
+                    User:
+                    <input type="text" id="<?php echo $this->plugin_name ?>-rsync-options[{{COUNT}}][user]" name="<?php echo $this->plugin_name ?>[rsync][{{COUNT}}][user]" value="{{USER}}" class="regular-text" placeholder="ec2-user etc..">
+                </label>
+            </li>
+            <li>
+                <label for="<?php echo $this->plugin_name ?>-rsync-options[{{COUNT}}][ssh_key]">
+                    <p>SSH Private Key:</p>
+                    <textarea id="<?php echo $this->plugin_name ?>-rsync-options[{{COUNT}}][ssh_key]" class="large-text code" name="<?php echo $this->plugin_name ?>[rsync][{{COUNT}}][ssh_key]">{{SSH_KEY}}</textarea>
+                </label>
+            </li>
+            <li>
+                <label for="<?php echo $this->plugin_name ?>-rsync-options[{{COUNT}}][dir]">
+                    Target Directory:
+                    <input type="text" id="<?php echo $this->plugin_name ?>-rsync-options[{{COUNT}}][dir]" name="<?php echo $this->plugin_name ?>[rsync][{{COUNT}}][dir]" class="regular-text" placeholder="~/public" value="{{DIR}}">
+                </label>
+            </li>
+            <li>
+                <label for="<?php echo $this->plugin_name ?>-rsync-options[{{COUNT}}][rsync_options]">
+                    Additional rsync options:
+                    <input type="text" id="<?php echo $this->plugin_name ?>-rsync-options[{{COUNT}}][rsync_options]" name="<?php echo $this->plugin_name ?>[rsync][{{COUNT}}][rsync_options]" class="regular-text" placeholder="--exclude &quot;.git&quot;" value="{{RSYNC_OPTIONS}}">
+                </label>
+            </li>
+            <li>
+                <label for="<?php echo $this->plugin_name ?>-rsync-options[{{COUNT}}][before_command]">
+                    Before command:
+                    <input type="text" id="<?php echo $this->plugin_name ?>-rsync-options[{{COUNT}}][before_command]" name="<?php echo $this->plugin_name ?>[rsync][{{COUNT}}][before_command]" class="regular-text" placeholder="cp -r " value="{{BEFORE_COMMAND}}">
+                </label>
+                <p class="description">Command which will be executed before rsync</p>
+                <p class="description">
+                    {{ROOT}} : Document root (get_home_path())<br>
+                    {{WP_ROOT}} : WordPress root (ABSPATH)<br>
+                    {{OUTPUT_DIR}} : Static Maker output directory
+                </p>
+            </li>
+        </ul>
+    </script>
+
 </div>
 
 <?php
     add_action( 'admin_footer', 'Static_Maker\static_maker_javascript' );
 
-    function static_maker_javascript() { ?>
+    function static_maker_javascript() { global $rsync_options; ?>
     <script>
+        (function($) {
+            var replaceVars = function(html, vars) {
+                Object.keys(vars).forEach(function(key) {
+                    var reg = new RegExp(key, 'g');
+                    html = html.replace(reg, vars[key]);
+                });
+                return html;
+            };
+
+            var count = 0;
+            var rsyncVars = [
+                <?php foreach ( $rsync_options as $i => $rsync ): ?>
+                <?php echo $i !== 0 ? ',' : '' ?>{
+                    '{{HOST}}': '<?php echo $rsync['host'] ?>',
+                    '{{USER}}': '<?php echo $rsync['user'] ?>',
+                    '{{SSH_KEY}}': '<?php echo CryptoUtil::decrypt($rsync['ssh_key'], true) ?>',
+                    '{{DIR}}': '<?php echo $rsync['dir'] ?>',
+                    '{{RSYNC_OPTIONS}}': '<?php echo $rsync['rsync_options'] ?>',
+                    '{{BEFORE_COMMAND}}': '<?php echo $rsync['before_command'] ?>'
+                }
+                <?php endforeach; ?>
+            ];
+
+            rsyncVars.forEach(function(rsyncOpts) {
+                var html = $('#rsync-template').html();
+                html = html.replace(/{{COUNT}}/g, count);
+                html = replaceVars(html, rsyncOpts);
+                var $html = $(html);
+                count++;
+
+                var $source = $('[data-sm-source="rsync"]').last();
+                if ($source.length) {
+                    $html.insertAfter($source);
+                } else {
+                    $('.rsync-list').prepend($html);
+                }
+            });
+
+            $('.add-target').on('click', function(e) {
+                var target = $(this).data('sm-target');
+                var $source = $('[data-sm-source="' + target + '"]').last();
+                var html = $('#rsync-template').html();
+                html = html.replace(/{{COUNT}}/g, count);
+                html = replaceVars(html, Object.keys(rsyncVars[0]).reduce(function(a, c) { a[c] = ''; return a; }, {}));
+                var $html = $(html);
+                count++;
+
+                if ($source.length) {
+                    $html.insertAfter($source);
+                } else {
+                    $('.rsync-list').prepend($html);
+                }
+            });
+
+            $('.remove-target').on('click', function() {
+                var target = $(this).data('sm-target');
+                $('[data-sm-source="' + target + '"]').last().remove();
+            });
+        }) (jQuery);
     </script>
-<?php }
+<?php };
