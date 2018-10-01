3535b6f99e545965dedf6eae86c137ac330e7fd8
ameyamashiro <yamashiro@ryukyu-i.co.jp>
2018-10-01 12:37:37 +0900

    add test base

---
+++ tests/test-sample.php
@@ -0,0 +1,12 @@
+<?php
+declare (strict_types = 1);
+
+use PHPUnit\Framework\TestCase;
+
+final class SampleTest extends TestCase
+{
+    public function testSample()
+    {
+        $this->assertTrue(true);
+    }
+}
