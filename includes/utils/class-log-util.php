046ba589c551b90bd5f003e1610231431bd57c19
ameyamashiro <yamashiro@ryukyu-i.co.jp>
2018-11-30 15:11:59 +0900

    update

--- includes/utils/class-log-util.php
+++ includes/utils/class-log-util.php
@@ -1,11 +1,38 @@
 <?php
 namespace Static_Maker;
 
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+
 class LogUtil
 {
+    private static $instance = null;
+    public $log;
+
+    private function __construct()
+    {
+        $this->log = new Logger('StaticMaker');
+        if (WP_DEBUG) {
+            $stream = new RotatingFileHandler(self::get_log_path(), 0, Logger::DEBUG);
+        } else {
+            $stream = new RotatingFileHandler(self::get_log_path(), 0, Logger::NOTICE);
+        }
+        $this->log->pushHandler($stream);
+    }
+
+    public static function get_instance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new LogUtil();
+        }
+
+        return self::$instance;
+    }
+
     public static function get_log_path()
     {
-        $path = wp_upload_dir()['basedir'] . '/static-maker.log';
+        // $path = wp_upload_dir()['basedir'] . '/static-maker.log';
+        $path = get_home_path() . '/sm-logs/static-maker.log';
         $realpath = realpath($path);
         if ($realpath) {
             return $realpath;
@@ -15,14 +42,6 @@ class LogUtil
 
     public static function get_log_url()
     {
-        return str_replace(untrailingslashit(get_home_path()), '', self::get_log_path());
-    }
-
-    public static function write_with_trace($content)
-    {
-        $path = self::get_log_path();
-        $trace = debug_backtrace()[1];
-        $prepend = $trace['class'] . $trace['type'] . $trace['function'];
-        return file_put_contents($path, $prepend . ' : ' . $content . "\n", FILE_APPEND);
+        return str_replace('//', '/', str_replace(untrailingslashit(get_home_path()), '', self::get_log_path()));
     }
 }
